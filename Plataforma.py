3b6240645fd19d24a28af0a49e2884c628bdb062
Paula Sanchez Velez <psanchezvelez03@gmail.com>
2024-11-30 11:23:05 +0100

    Clase Plataforma

---
+++ Plataforma.py
@@ -0,0 +1,12 @@
+import pygame
+
+import constantes
+
+
+class Plataforma:
+    def __init__(self, pos_x, ancho):
+        self.rect = pygame.Rect(pos_x, constantes.POSICION_J_Y, ancho, constantes.ALTO_PLATAFORMA)
+
+    def draw(self, pantalla):
+        pygame.draw.rect(pantalla, constantes.COLOR_PLATAFORMA, self.rect)
+
