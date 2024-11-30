eafbb9cead1cd69fa3613c7ba31d8d4ef03db841
Paula Sanchez Velez <psanchezvelez03@gmail.com>
2024-11-30 11:02:59 +0100

    Clase Jugador (hay que comprobar el funcionamiento aún)

---
+++ Jugador.py
@@ -0,0 +1,74 @@
+import constantes
+from Utils import Utils
+from constantes import  POSICION_J_X, POSICION_J_Y
+
+
+class Jugador:
+
+    def __init__(self):
+
+        # Atributos de la animacion
+        self.cronometro_animacion = 0
+        self.velocidad_animacion = constantes.VELOCIDAD_ANIMACION
+        self.indice_animacion = 0
+
+        # Mapa con las animaciones disponibles
+        self.animaciones = {
+            "caminando":Utils.load_animation(constantes.RUTA_DINO_CAMINANDO, 6, constantes.ESCALA),
+            "reposo": Utils.load_animation(constantes.RUTA_DINO_REPOSO, 4, constantes.ESCALA),
+            "muriendo": Utils.load_animation(constantes.RUTA_DINO_MURIENDO, 3, constantes.ESCALA)
+        }
+
+        # Posicion del jugador
+        self.posicion_jugador_x = POSICION_J_X
+        self.posicion_jugador_y = POSICION_J_Y
+
+        # Estado del jugador
+        self.moviendose = False
+
+        # Array de imagenes (ruta a las imagenes) de la animacion que se este reproduciendo
+        self.animacion_actual = self._get_animacion("reposo")
+
+        self.rect = self.animacion_actual[self.indice_animacion].get_rect()
+
+
+
+
+    def draw(self, screen):
+        screen.blit(self.animacion_actual[self.indice_animacion], self.rect)
+
+    def cambiar_animacion(self, accion):
+        """
+        Cambia la animación actual del jugador según la acción.
+        :param accion: str - Puede ser 'caminando', 'reposo', 'muriendo'.
+        """
+        self.animacion_actual = self._get_animacion(accion)
+        self.indice_animacion = 0  # Reinicia la animación al primer frame
+
+
+    def _get_animacion(self, accion):
+
+        if accion == "caminando":
+            return self.animaciones.get("caminando")
+        elif accion == "reposo":
+            return self.animaciones.get("reposo")
+        else:
+            return self.animaciones.get("muriendo")
+
+
+
+    def actualizar_animacion(self):
+        # Obtiene la velocidad a la que ira la animacion
+        self.cronometro_animacion = self._calcular_velocidad_animacion()
+
+        # Segun el tipo de animacion se reiniciara cuando terminen las imagenes
+        if self.cronometro_animacion >= 1:
+            self.indice_animacion = (self.indice_animacion + 1) % len(self.animacion_actual)
+            self.cronometro_animacion = 0
+
+
+    def _calcular_velocidad_animacion(self):
+        if not self.moviendose:
+            return self.velocidad_animacion / 2
+        else:
+            return self.velocidad_animacion
